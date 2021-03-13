42d09d794c89b29d45105533d996aa84d9e1b789
k-yomo <kanji.yy@gmail.com>
2021-03-14 03:02:59 +0900

    Use single quote for TypeScript

--- web_client/pages/_app.tsx
+++ web_client/pages/_app.tsx
@@ -1,6 +1,6 @@
-import React from "react"
-import { AppProps } from "next/app";
-import "../styles/globals.css";
+import React from 'react';
+import { AppProps } from 'next/app';
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
