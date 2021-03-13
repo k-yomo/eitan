a62d394e5148b1a09a34c180d6fcd5a7b60d23a5
k-yomo <kanji.yy@gmail.com>
2021-03-14 02:44:28 +0900

    Initialize web client

---
+++ web_client/.eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+    root: true,
+    parser: "@typescript-eslint/parser",
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+        },
+        ecmaVersion: 2018,
+        sourceType: "module",
+    },
+    extends: [
+        "eslint:recommended",
+        "plugin:react/recommended",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:@typescript-eslint/eslint-recommended",
+        "prettier"
+    ],
+    env: {
+        browser: true,
+        es6: true,
+    },
+    globals: {
+        Atomics: "readonly",
+        SharedArrayBuffer: "readonly",
+    },
+    plugins: ["react", "react-hooks", "@typescript-eslint"],
+    rules: {
+        "@typescript-eslint/explicit-module-boundary-types": 0,
+        "@typescript-eslint/no-var-requires": 0,
+    },
+    settings: {
+        react: {
+            version: "detect",
+        },
+    },
+};
