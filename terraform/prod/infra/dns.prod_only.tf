678dcc676484b8ba4465c2ef924c138866ce16e1
k-yomo <kanji.yy@gmail.com>
2021-03-15 00:29:15 +0900

    Add dns records

--- terraform/prod/infra/dns.prod_only.tf
+++ terraform/prod/infra/dns.prod_only.tf
@@ -9,3 +9,23 @@ resource "google_dns_managed_zone" "eitan_flash_com" {
 output "dns_eitan_flash_com_name_servers" {
   value = google_dns_managed_zone.eitan_flash_com.name_servers
 }
+
+resource "google_dns_record_set" "eitan_flash_com" {
+  name = google_dns_managed_zone.eitan_flash_com.dns_name
+  type = "A"
+  ttl  = 86400
+
+  managed_zone = google_dns_managed_zone.eitan_flash_com.name
+
+  rrdatas = ["76.76.21.21"]
+}
+
+resource "google_dns_record_set" "www_eitan_flash_com" {
+  name = "www.${google_dns_managed_zone.eitan_flash_com.dns_name}"
+  type = "CNAME"
+  ttl  = 86400
+
+  managed_zone = google_dns_managed_zone.eitan_flash_com.name
+
+  rrdatas = ["cname.vercel-dns.com."]
+}
