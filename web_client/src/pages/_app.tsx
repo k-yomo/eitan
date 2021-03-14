b1ad7d647f526321f458c8322f485994020f7c93
k-yomo <kanji.yy@gmail.com>
2021-03-15 00:33:33 +0900

    Add noindex tag

--- web_client/src/pages/_app.tsx
+++ web_client/src/pages/_app.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
 import { AppProps } from 'next/app';
+import Head from 'next/head';
 import 'tailwindcss/tailwind.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <meta name="robots" content="noindex,nofollow,noarchive" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
