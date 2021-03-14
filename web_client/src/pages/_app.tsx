a2f5e7930e7ebe181dce9358e0be7b0bdf6c6393
k-yomo <kanji.yy@gmail.com>
2021-03-15 01:54:25 +0900

    Implement header component

--- web_client/src/pages/_app.tsx
+++ web_client/src/pages/_app.tsx
@@ -2,13 +2,33 @@ import React from 'react';
 import { AppProps } from 'next/app';
 import Head from 'next/head';
 import 'tailwindcss/tailwind.css';
+import Header from '../components/Header';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
         <meta name="robots" content="noindex,nofollow,noarchive" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
       </Head>
+      <Header />
       <Component {...pageProps} />
     </>
   );
--- web_client/src/pages/index.tsx
+++ web_client/src/pages/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
 
-export default function Home() {
+export default function HomePage() {
   return <div>home</div>;
 }
